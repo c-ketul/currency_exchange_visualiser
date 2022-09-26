1d998e5aac447da127ee3b10bf8485a1a55c2f18
vaibhav <vaibhavpaliwal620@gmail.com>
2022-09-27 02:02:42 +0530

    Min Req Implemented

--- frontend/js/custom.js
+++
@@ -1,6 +0,0 @@
-const api = "https://api.exchangerate-api.com/v4/latest/USD";
-function getResult() {
-    fetch(`${api}`).then(currency=>{
-        return currency.json();
-    }).then(displayResults);
-}
--- main.js
+++ main.js
@@ -1,22 +1,15 @@
 let period = 5
 let searchQuery = ""
-let smallData = [];
 const dDate = data.Date;
+let smallData = [];
 let smallDataToUse =[]
 
-// console.log(data);
+
 document.querySelector("#searchBtn").addEventListener("click", () => {
   smallData = []
   searchQuery = document.querySelector("#searchQuery").value;
-  console.log(searchQuery);
   if (searchQuery.length != 0) {
-    // lineChart();
     let currencyData = data[searchQuery];
-    // console.log("Zero : ",currencyData); // this is working
-    // console.log("crr Data len is : ",dataDate.size);
-    // console.log("WHo date",dataDate);
-    // console.log("crr Data len is : ",currencyData.size);
-    // console.log("cur Data",currencyData);
     var curSize = Object.keys(currencyData).length;
     for(var i=0;i<curSize;i++){
       if(currencyData[i] != null){
@@ -64,58 +57,37 @@ document.querySelectorAll(".btn").forEach((button)=>{
   })
 });
 
-
-
 document.querySelectorAll(".btn-year").forEach((button)=>{
   button.addEventListener("click",()=>{
     smallDataToUse = [];
-    // var yr1 = Number(button.value.slice(3,2));
-    // console.log(button.value);
-    // var yr1 = button.value.toString().slice(2,2);
 
-    var yr1 = (button.value.toString()[3] + button.value.toString()[4]);
-    console.log(yr1);
+    var startYear = button.value;
     var flag = false;
-    // console.log("its small",smallData.length);
     for(i=0;i<smallData.length;i++){
-      var curYear = smallData[i].x.toString()[13]+smallData[i].x.toString()[14];
-      if(curYear==yr1){
+      var curYear = smallData[i].x.toString()[13] + smallData[i].x.toString()[14];
+      if(curYear == startYear){
         flag = true;
-        console.log("is in??");
-        while( i < smallData.length &&  (smallData[i].x.toString()[13]+smallData[i].x.toString()[14])==yr1){
+        while( i < smallData.length &&  (smallData[i].x.toString()[13]+smallData[i].x.toString()[14]) == startYear){
           var obj = {
             x : smallData[i].x,
             y : smallData[i].y
           }
           smallDataToUse.push(obj);
-          console.log("in??");
           i++;
         }
       }
-      if(flag)break;
+      if(flag) break;
     }
     if(smallDataToUse.length != 0){
-        console.log("WHO??");
         yearLineChart();
     }
   });
 });
 
+
 function getData(){
     let dataToUse = [];
 
-    // let currencyData = data[searchQuery];
-    // let dateData = data.Date;
-    // for(let i=0; i<currencyData.length; i+=period){
-    //     if(currencyData[i] != null){
-    //         var obj = {
-
-    //         }
-    //         obj.x.push( currencyData[i] );
-    //         obj.y.push( dateData[i] );
-
-    //     }
-    // }
     console.log("small data is : ",smallData.length);
     for(var i=0;i<smallData.length;i+=period){
       const obj = {
@@ -124,15 +96,12 @@ function getData(){
       }
       dataToUse.push(obj);
     }
-    // console.log();
-    // console.log("getData()",dataToUse);
     return dataToUse;
 }
 
 function lineChart() {
   let plotData = getData();
 
-  // console.log(plotData)
   let chart = new CanvasJS.Chart("chartContainer", {
     axisX: {
       interval: 1,
@@ -155,7 +124,6 @@ function lineChart() {
 
 function yearLineChart() {
   let plotData = smallDataToUse;
-  // console.log("night",plotData)
   let chart = new CanvasJS.Chart("chartContainer", {
     axisX: {
       interval: 1,
@@ -175,4 +143,3 @@ function yearLineChart() {
   });
   chart.render();
 };
-
